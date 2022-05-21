da5d5a730ee9ee8407a33b29dd11c475add6b266
deyanix <deyanix@deyanix.eu>
2022-05-21 18:10:55 +0200

    Dodano mechanizm liczenia czasu gry

--- src/main/java/pl/edu/pw/elka/prm2t22l/battleships/GamePlayManager.java
+++ src/main/java/pl/edu/pw/elka/prm2t22l/battleships/GamePlayManager.java
@@ -1,15 +1,55 @@
 package pl.edu.pw.elka.prm2t22l.battleships;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.Stack;
 
 public class GamePlayManager {
-	private Board board;
+	private final Board board;
 	private final Stack<Turn> turns = new Stack<>();
+	private LocalDateTime startTime;
+	private Duration passedTime = Duration.ZERO;
+
+	public GamePlayManager(Board board) {
+		this.board = board;
+	}
+
+	public Board getBoard() {
+		return board;
+	}
+
+	public LocalDateTime getStartTime() {
+		return startTime;
+	}
+
+	public void start() {
+		startTime = LocalDateTime.now();
+	}
+
+	public void pause() {
+		passedTime = getCurrentTime();
+		startTime = null;
+	}
+
+	public Duration getCurrentTime() {
+		if (startTime == null) {
+			return passedTime;
+		}
+		return Duration.between(startTime, LocalDateTime.now()).plus(passedTime);
+	}
+
+	public Duration getPassedTime() {
+		return passedTime;
+	}
 
 	public void turn(Location location, FieldState state) {
 		FieldState previousState = board.getFieldState(location);
 		board.setFieldState(location, state);
 		turns.push(new Turn(location, previousState, state));
+	}
+
+	public void turn(int x, int y, FieldState state) {
+		turn(new Location(x, y), state);
 	}
 
 	public boolean rollbackTurn() {
--- src/main/java/pl/edu/pw/elka/prm2t22l/battleships/Main.java
+++ src/main/java/pl/edu/pw/elka/prm2t22l/battleships/Main.java
@@ -3,31 +3,38 @@ package pl.edu.pw.elka.prm2t22l.battleships;
 public class Main {
 	public static void main(String[] args) {
 		Board board = Board.createEmptyBoard(3, 3);
-		// EWB
-		// EWB
-		// BEB
-		board.display();
 		board.setFieldState(0, 0, FieldState.EMPTY);
 		board.setFieldState(1, 0, FieldState.WATER);
 		board.setFieldState(2, 0, FieldState.BATTLESHIP);
-
 		board.setFieldState(0, 1, FieldState.EMPTY);
 		board.setFieldState(1, 1, FieldState.WATER);
 		board.setFieldState(2, 1, FieldState.BATTLESHIP);
-
 		board.setFieldState(0, 2, FieldState.BATTLESHIP);
 		board.setFieldState(1, 2, FieldState.EMPTY);
 		board.setFieldState(2, 2, FieldState.BATTLESHIP);
 
-		for (int i = 0; i < board.getWidth(); i++) {
-			System.out.printf("Column %d: %d%n", i, board.getFieldsInColumnByState(FieldState.BATTLESHIP, i));
-		}
 
-		for (int i = 0; i < board.getHeight(); i++) {
-			System.out.printf("Row %d: %d%n", i, board.getFieldsInRowByState(FieldState.BATTLESHIP, i));
-		}
+		new Thread() {
+			@Override
+			public void run() {
+				try {
+					GamePlayManager manager = new GamePlayManager(board);
+					manager.start();
+					Thread.sleep(2000);
+					System.out.println(manager.getPassedTime());
+					System.out.println(manager.getCurrentTime());
+					manager.pause();
+					manager.start();
+					Thread.sleep(2000);
+					System.out.println(manager.getPassedTime());
+					System.out.println(manager.getCurrentTime());
+				} catch (InterruptedException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}.start();
 
-		//boardGenTest();
+		boardGenTest();
 	}
 
 	public static void boardGenTest(){
