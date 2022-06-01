18c8ff31703e8c51543b41b0f46619e32eee6181
MBanzerberg <bartekmilkowski4@gmail.com>
2022-06-01 21:13:30 +0200

    Próba rozbudowania o zmianę koloru pola po wciśnięciu myszą obszaru.

--- src/main/java/pl/edu/pw/elka/prm2t22l/battleships/BoardField.java
+++ src/main/java/pl/edu/pw/elka/prm2t22l/battleships/BoardField.java
@@ -1,20 +1,28 @@
 package pl.edu.pw.elka.prm2t22l.battleships;
 
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.*;
 
-public class BoardField extends JPanel {
+public class BoardField extends JPanel implements MouseListener {
     private final int fieldSize;
     private final int startPointX;
     private final int startPointY;
     private final int boardSize;
+    private int flag = 0;
+    List<Point> positions;
 
     BoardField() {
-        setPreferredSize(new Dimension(400, 400));
-        this.fieldSize = 60;
-        this.startPointX = 60;
-        this.startPointY = 60;
-        this.boardSize = 4;
+        addMouseListener(this);
+        this.positions = new ArrayList<>();
+        setPreferredSize(new Dimension(1200, 1200));
+        this.fieldSize = 70;
+        this.startPointX = 100;
+        this.startPointY = 40;
+        this.boardSize = 6;
     }
 
     @Override
@@ -27,14 +35,66 @@ public class BoardField extends JPanel {
                 g2d.drawRect(startPointX+ ((fieldSize/2)*j), startPointY + ((fieldSize/2)*i), this.fieldSize, this.fieldSize);
             }
         }
+        drawRectangles(g2d);
+    }
+
+    private void drawRectangles(Graphics2D g2d) {
+        int x, y;
+        g2d.setColor(Color.blue);
+        for (Point point : positions) {
+            x = (int) point.getX();
+            y = (int) point.getY();
+            int multiplicatorX = getMultiplicatorX(x)/2;
+            int multiplicatorY = getMultiplicatorY(y)/2;
+            g2d.fillRect(startPointX + multiplicatorX*fieldSize, startPointY + multiplicatorY*fieldSize, this.fieldSize/2,this.fieldSize/2);
+        }
+    }
+
+    private int getMultiplicatorX(int x) {
+        if(x<startPointX) return 0;
+        int multipX = x / this.fieldSize;
+        return multipX;
+    }
+
+    private int getMultiplicatorY(int y) {
+        if(y<startPointX) return 0;
+        int multipY = y / this.fieldSize;
+        return multipY;
     }
 
     public static void main(String[] args) {
-        BoardField b = new BoardField();
         JFrame f = new JFrame();
-        f.add(b);
+        f.add(new BoardField());
         f.setSize(400,400);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setVisible(true);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        int x = e.getX();
+        int y = e.getY();
+        positions.add(new Point(x,y));
+        repaint();
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }
