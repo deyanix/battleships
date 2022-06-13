8452ebb4c2c3ccd9cba134580c885f29d0951875
szymsioH <93218007+szymsioH@users.noreply.github.com>
2022-06-13 15:46:06 +0200

    Dalsze prace nad klasą InterfaceConfigWindow

--- src/main/java/pl/edu/pw/elka/prm2t22l/battleships/gui/InterfaceConfigWindow.java
+++ src/main/java/pl/edu/pw/elka/prm2t22l/battleships/gui/InterfaceConfigWindow.java
@@ -2,6 +2,7 @@ package pl.edu.pw.elka.prm2t22l.battleships.gui;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.KeyEvent;
@@ -33,6 +34,8 @@ public class InterfaceConfigWindow extends JPanel {
         pDifficultyConfig.setBounds(0,200,200,200);
         pDifficultyConfig.setLayout(null);
 
+        //pDifficultyConfig.setBackground(Color.BLUE);
+
         JLabel lSelectDiffLvl = new JLabel("Select Difficulty Level:");
         lSelectDiffLvl.setBounds(20,0,150,25);
 
@@ -44,6 +47,7 @@ public class InterfaceConfigWindow extends JPanel {
             @Override
             public void itemStateChanged(ItemEvent e) {
                 diggicultyLevel = 1;
+                customSettingsEnabler = false;
             }
         });
 
@@ -56,6 +60,7 @@ public class InterfaceConfigWindow extends JPanel {
             @Override
             public void itemStateChanged(ItemEvent e) {
                 diggicultyLevel = 2;
+                customSettingsEnabler = false;
             }
         });
 
@@ -67,6 +72,8 @@ public class InterfaceConfigWindow extends JPanel {
             @Override
             public void itemStateChanged(ItemEvent e) {
                 diggicultyLevel = 3;
+                customSettingsEnabler = false;
+                System.out.println(customSettingsEnabler);
             }
         });
 
@@ -79,6 +86,8 @@ public class InterfaceConfigWindow extends JPanel {
             public void itemStateChanged(ItemEvent e) {
                 diggicultyLevel = 0;
                 customSettingsEnabler = true;
+
+                System.out.println(customSettingsEnabler);
             }
         });
 
@@ -101,22 +110,44 @@ public class InterfaceConfigWindow extends JPanel {
         pCustomConfig.setBounds(200,200,400,400);
         pCustomConfig.setLayout(null);
 
-        JLabel lTest = new JLabel("testets");
-        lTest.setBounds(0,0,20,20);
+        pCustomConfig.setBackground(Color.green);
 
-        pCustomConfig.add(lTest);
+        //JLabel lTest = new JLabel("testets");
+        //lTest.setBounds(0,0,20,20);
+        //pCustomConfig.add(lTest);
 
-        pCustomConfig.setVisible(customSettingsEnabler);
+        pCustomConfig.setVisible(customSettingsEnabler);  //wcześniej w tej linijce bylo: pCustomConfig.setVisible(customSettingsEnabler);
 //------PANEL Z PRZYCISKAMI---------------------------------------
         JPanel pButtonsPlace = new JPanel();
+        pButtonsPlace.setBounds(0,400,200,200);
+        pButtonsPlace.setLayout(null);
 
+        //pButtonsPlace.setBackground(Color.cyan);
 
+        JButton bSave = new JButton("Save");
+        bSave.setFocusable(false);
+        bSave.setBounds(30,40,67,40);
 
+        JButton bBack = new JButton("Back");
+        bBack.setFocusable(false);
+        bBack.setBounds(103,40,67,40);
 
+        JButton bPlay = new JButton("Play");
+        bPlay.setFocusable(false);
+        bPlay.setBounds(30,85,140,40);
+
+        pButtonsPlace.add(bSave);
+        pButtonsPlace.add(bBack);
+        pButtonsPlace.add(bPlay);
+
+        pButtonsPlace.setVisible(true);
+//----------------------------------------------------------------
         setSize(600,600);
         setLayout(null);
         add(pLogoPlace);
         add(pDifficultyConfig);
+        add(pCustomConfig);
+        add(pButtonsPlace);
 
         setVisible(true);
     }
