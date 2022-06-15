973c2b0a207d79bb24c8eda4ff12e8434498bc52
deyanix <deyanix@deyanix.eu>
2022-06-15 20:33:11 +0200

    Zmiana Main

--- src/main/java/pl/edu/pw/elka/prm2t22l/battleships/Main.java
+++ src/main/java/pl/edu/pw/elka/prm2t22l/battleships/Main.java
@@ -1,29 +1,10 @@
 package pl.edu.pw.elka.prm2t22l.battleships;
 
-import pl.edu.pw.elka.prm2t22l.battleships.entity.Location;
-import pl.edu.pw.elka.prm2t22l.battleships.entity.ShipType;
-import pl.edu.pw.elka.prm2t22l.battleships.generator.BoardGenerator;
-
-import javax.swing.*;
-import java.util.concurrent.ThreadLocalRandom;
+import pl.edu.pw.elka.prm2t22l.battleships.gui.FrameInterface;
 
 public class Main {
 	public static void main(String[] args) {
-		GameConfiguration configuration = new GameConfiguration();
-		configuration.setBoardSize(7, 6);
-		configuration.setShipAmount(ShipType.LONG, 1);
-		configuration.setShipAmount(ShipType.MEDIUM, 2);
-		configuration.setShipAmount(ShipType.SHORT, 3);
-		configuration.setNumberOfVisibleFields(4);
-		configuration.setSeed(10);
-		GamePlayManager manager = new GamePlayManager(configuration);
-		manager.createBoard();
-
-		JFrame frame = new JFrame();
-		frame.setSize(600,600);
-		frame.setContentPane(new BoardComponent(manager));
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
+		new FrameInterface();
 	}
 
 }
